950a6df50e6da6bec3f70d2be2dac71f1ecc96b0
xcymo <361855254@qq.com>
2018-12-10 13:18:32 +0800

    2018-12-10

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 import MyComponent1 from './xcy1';
 import MyComponent2 from './xcy2';
 import MyComponent3 from './xcy3';
@@ -11,17 +11,25 @@ ReactDOM.render(
     // exact 精确匹配
     // 404 页面必须要写在最后，因为是按照从上到下匹配。并且用Switch组件包起来
     // Redirect 也要在Switch里面
-    <Router>
+    // Router basename 层级关系
+    // forceRefresh 强制刷新，没听懂，先关掉
+    /*
+        几种路由模式： 1. BrowserRouter 缺点：刷新后会找不到页面但是URL比较漂亮；
+                    2.  HashRouter 缺点：URL不漂亮，带个#； 优点： 刷新后可以直接看
+                    3.  MemoryRouter 缺点： 不能前进后退； 优点：URL漂亮
+                    4. NativeRouter react-native使用
+                    5. StaticRouter 静态路由，配合后台使用，服务端渲染
+    */
+    <HashRouter basename="demo" forceRefresh={false}>
         <div>
             <NavBar />
             <Switch>
                 <Route path="/" exact component={MyComponent1} />
                 <Route path="/xcy2" component={MyComponent2} />
-                <Route path="/xcy3" component={MyComponent3} />
+                <Route path="/xcy3/:a/:b" component={MyComponent3} />
                 <Redirect from="/redirect1" to="/xcy3" />
                 <Route component={Error} />
             </Switch>
         </div>
-
-    </Router>
+    </HashRouter>
     , document.getElementById('app'))
